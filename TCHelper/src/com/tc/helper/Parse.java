65c86a4f54c667873d6745cfdc91aab633bf24d4
yeonwoojeong <todriver08@nate.com>
2017-06-12 14:47:26 +0900

    ui 완성

--- TCHelper/src/com/tc/helper/Parse.java
+++ TCHelper/src/com/tc/helper/Parse.java
@@ -1,5 +1,95 @@
 package com.tc.helper;
+ 
+import java.io.File;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+ 
+@FixMethodOrder (MethodSorters.NAME_ASCENDING)
+ 
 public class Parse {
+    private static WebDriver driver;
+	
+	static String receiptNum="";
+	static String url="https://kor.tellburgerking.com";
+	static int speed_int = 100;
+	
+	public void inputNumber(String number, String txt_speed){
+	      // 텍스트 필드값 가져오기
+		receiptNum = number;
+		System.out.println("speed.equals "+txt_speed );
+		speed_int = Integer.parseInt(txt_speed);	
+		
+		
+	}
+	
+ 
+    @BeforeClass
+    public static void setUp() throws Exception {
+    	
+    	File file = new File("c:/chromedriver.exe");
+    	File file2 = new File("d:/chromedriver.exe");
+   		if(file.isFile()){         
+   			System.setProperty("webdriver.chrome.driver", "c:/chromedriver.exe"); //크롬 드라이버 파일 경로설정
+		}else if(file2.isFile()){
+			System.setProperty("webdriver.chrome.driver", "d:/chromedriver.exe"); //크롬 드라이버 파일 경로설정	
+		}else{
+			//안내문
+			runner.labelIntroduce.setText("chromedriver가 없습니다.\n");
+		}
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+		driver.get(url); 
+		 driver.manage().window().maximize();
+//	    System.setProperty("selenide.browser", "Chrome");
+//	    open(url);
+		
 
+    }
+    
+    public static void login() throws InterruptedException   {
+    	boolean boo = true;
+    	WebElement NextButton = driver.findElement(By.id("NextButton"));
+    	NextButton.click();   
+    	
+        driver.findElement(By.id("CN1")).sendKeys(receiptNum);  //ID
+		Thread.sleep(1000);
+		driver.findElement(By.id("NextButton")).click();
+        
+		
+		int i = 0;
+        while(boo){
+        	try{
+
+        	}catch (NoSuchElementException e){
+        		
+        	}
+           
+ 
+        }
+        
+    }
+    
+ 
+    @Test
+    public static void run() throws Exception {
+    	login();
+		Thread.sleep(500);
+    }
+ 
+    @AfterClass
+    public static void tearDown() throws Exception {
+        driver.quit();
+    }
+ 
 }
--- TCHelper/src/com/tc/helper/Ui.java
+++ TCHelper/src/com/tc/helper/Ui.java
@@ -13,19 +13,22 @@ class runner extends Thread{
 	Ui simul = new Ui();
 	public static    JLabel labelIntroduce;
 //	public static survey survey = new survey();
-	public  static  JTextField tf_btsUrl, note_row;;
+	public  static  JTextField tf_btsUrl, note_row,result_row_start,result_row_end;	
 	public static JLabel submit,number;
 	public boolean check = true;
 
 	
 	public void run(){
         // 텍스트 필드값 가져오기
-        String receiptNum = tf_btsUrl.getText();
-    	String txt_speed = note_row.getText();
-        if(txt_speed.equals("")){
-        	txt_speed = "100";
+        String txt_btsUrl = tf_btsUrl.getText();
+    	String txt_note_row = note_row.getText();
+    	String txt_result_row_start = result_row_start.getText();
+    	String txt_result_row_end = result_row_end.getText();
+    	
+        if(txt_note_row.equals("")){
+        	txt_note_row = "100";
         }else{
-        	txt_speed = note_row.getText();
+        	txt_note_row = note_row.getText();
         }
          
         try {
@@ -51,7 +54,7 @@ class JPanel033 extends JPanel{        // 3번째 패널
 	// 클래스 멤버 필드 설정
 	
 	private    JLabel name;
-    private    JLabel url, note;
+    private    JLabel url, note,row,and;
     private    JTextField tf_speed;
     private JLabel image;
     private Font f1, f2, f3;
@@ -75,42 +78,65 @@ class JPanel033 extends JPanel{        // 3번째 패널
         name.setSize(200, 30);   
         name.setLocation(10, 0);
         
-        note = new JLabel("비고 열 지정");
+        note = new JLabel("비고 열 지정 : ");
         note.setSize(230, 20);   
-        note.setLocation(165, 50);
+        note.setLocation(10, 50);
         add(note);
         
         runner.note_row = new JTextField();
-        runner.note_row.setBounds(250, 50,50, 20);
-        runner.note_row.setText("100");
+        runner.note_row.setBounds(100, 50,50, 20);
+        runner.note_row.setText("비고열");
         add(runner.note_row);
-
+        
+        
+        
+        row = new JLabel("변경할 열의 범위 지정 : ");
+        row.setSize(230, 50);   
+        row.setLocation(10, 60);
+        add(row);
+        
+        runner.result_row_start = new JTextField();
+        runner.result_row_start.setBounds(150, 75,30,20);
+        runner.result_row_start.setText("시작");
+        add(runner.result_row_start);
+        
+        and = new JLabel("~");
+        and.setSize(230, 50);   
+        and.setLocation(185, 60);
+        add(and);
+        
+        runner.result_row_end = new JTextField();
+        runner.result_row_end.setBounds(200, 75,30,20);
+        runner.result_row_end.setText("끝");
+        add(runner.result_row_end);
+        
 //        image.setSize(40,50);
 //        image.setBounds(310,5,60,60);
         
-        url = new JLabel("BTS 필터 URL: ");
+        url = new JLabel("필터 목록 URL : ");
         url.setBounds(10,30,100,20);
         
         // 텍스트 필드
         runner.tf_btsUrl = new JTextField();             
         runner.tf_btsUrl.setBounds(100,30,200,20);
+        runner.tf_btsUrl.setText("url");
         
-        thread.labelIntroduce = new JLabel("결과");
-        thread.labelIntroduce.setBounds(10,50,280,20);
+//        thread.labelIntroduce = new JLabel("결과");
+//        thread.labelIntroduce.setBounds(10,50,280,20);
        
             
         // 버튼        
         buttonSave = new JButton("시작");
-        buttonSave.setBounds(80,80,100,20);
+        buttonSave.setBounds(80,110,100,20);
         buttonSave.addActionListener(new EventHandlerSave());   
         
         buttonStop = new JButton("정지");
-        buttonStop.setBounds(220,80,100,20);
+        buttonStop.setBounds(220,110,100,20);
         buttonStop.addActionListener(new EventHandlerStop());   
         
         add(name);
       
-        add(thread.labelIntroduce);
+//        add(thread.labelIntroduce);
         
         add(url);
         add(thread.tf_btsUrl);
@@ -166,7 +192,7 @@ public class Ui extends JFrame{
 
         win.add(win.jpanel03);
 
-        win.setSize(400,150);
+        win.setSize(400,180);
         win.setVisible(true);
         win.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
